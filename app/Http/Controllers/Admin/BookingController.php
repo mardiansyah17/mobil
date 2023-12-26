88e085d863d973da6b5a70de436c909f909ceb99
Mardiansyah17 <mardiansyahm002@gmail.com>
2023-12-26 08:33:25 +0700

    up

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -7,6 +7,7 @@ use App\Models\Type;
 use App\Models\User;
 use App\Models\Brand;
 use App\Models\Booking;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
@@ -19,18 +20,37 @@ use App\Http\Requests\BookingUpdateRequest;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
         if (request()->ajax()) {
             $query = Booking::with(['user', 'item.brand']);
 
+            if (request()->get('statusBooking')) {
+                $query->where('status', '=', request()->get('statusBooking'));
+            }
+
+            if (request()->get('statusBayar')) {
+                $query->where('payment_status', '=', request()->get('statusBayar'));
+            }
+
+            if (request()->get('startDate')) {
+                $query->where('start_date', '=', request()->get('startDate'));
+            }
+
+            if (request()->get('endDate')) {
+                $query->where('end_date', '=', request()->get('endDate'));
+            }
+
+
             return DataTables::of($query)
+                ->addColumn('start_date', function ($booking) {
+                    return Carbon::parse($booking->start_date)->format('d-m-Y');
+                })
+                ->addColumn('end_date', function ($booking) {
+                    return Carbon::parse($booking->end_date)->format('d-m-Y');
+                })
                 ->addColumn('action', function ($booking) {
                     return '
                         <a class="block w-full px-2 py-1 mb-1 text-xs text-center text-white transition duration-500 bg-gray-700 border border-gray-700 rounded-md select-none ease hover:bg-gray-800 focus:outline-none focus:shadow-outline"
@@ -53,11 +73,7 @@ class BookingController extends Controller
         return view('admin.bookings.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
@@ -66,7 +82,7 @@ class BookingController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -77,7 +93,7 @@ class BookingController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -88,7 +104,7 @@ class BookingController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit(Booking $booking)
@@ -107,8 +123,8 @@ class BookingController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(BookingUpdateRequest $request, Booking $booking)
@@ -124,7 +140,7 @@ class BookingController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy(Booking $booking)
--- resources/views/admin/bookings/index.blade.php
+++ resources/views/admin/bookings/index.blade.php
@@ -1,128 +1,182 @@
 <x-app-layout>
-  <x-slot name="title">Admin</x-slot>
-  <x-slot name="header">
-    <h2 class="text-xl font-semibold leading-tight text-gray-800">
-      {{ __('Booking') }}
-    </h2>
-  </x-slot>
+    <x-slot name="title">Admin</x-slot>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Booking') }}
+        </h2>
+    </x-slot>
+    <div class="bg-white rounded-sm p-3 shadow-md w-[90%] mt-3 mx-auto">
+        <div class="flex justify-around">
+            <select class="form-select px-4 py-3 rounded-sm " id="statusBooking">
+                <option value="" selected>Status Booking</option>
 
-  <x-slot name="script">
-    <script>
-      // AJAX DataTable
-      var datatable = $('#dataTable').DataTable({
-        processing: true,
-        serverSide: true,
-        stateSave: true,
-        ajax: {
-          url: '{!! url()->current() !!}',
-        },
-        language: {
-          url: '//cdn.datatables.net/plug-ins/1.12.1/i18n/id.json'
-        },
-        
-        columns: [
-          //{ className: 'dt-center', targets: '_all' },
-          {
-            data: 'id',
-            name: 'id',
-          },
-          {
-            data: 'user.name',
-            name: 'user.name',
-            // orderable: false,
-            // searchable: false,
-            // width: '20%',
-          },
-          {
-            data: 'name',
-            name: 'name',
-            // orderable: false,
-            // searchable: false,
-            // width: '20%',
-          },
-          {
-            data: 'item.brand.name',
-            name: 'item.brand.name'
-          },
-          
-          {
-            data: 'item.name',
-            name: 'item.name'
-          },
-          {
-            data: 'start_date',
-            name: 'start_date',
-            width: '5%',
+                <option value="pending">Pending
+                </option>
+                <option value="confirmed">Confirmed
+                </option>
+                <option value="done">Done</option>
+            </select>
+            <select class="form-select px-4 py-3 rounded-sm" id="statusBayar">
+                <option value="" selected>Status Bayar</option>
+                <option value="pending">Pending
+                </option>
+                <option value="success">Success
+                </option>
+                <option value="failed">Failed</option>
+            </select>
 
-          },
-          {
-            data: 'end_date',
-            name: 'end_date',
-            width: '5%',
+            <div>
+                <input type="date" class="form-input" id="startDate">
+                <span class="mx-2">-</span>
+                <input type="date" class="form-input" id="endDate">
+            </div>
 
-          },
-          {
-            data: 'status',
-            name: 'status'
-
-          },
-          {
-            data: 'payment_status',
-            name: 'payment_status'
-
-          },
-          {
-            data: 'total_price',
-            name: 'total_price',
-            width: '10%',
-            render : $.fn.dataTable.render.number( '.', ',', 0, 'Rp ' ),
-
-          },
-          {
-            data: 'action',
-            name: 'action',
-            orderable: false,
-            searchable: false,
-            width: '15%'
-          },
-        ],
-      });
-    </script>
-  </x-slot>
-
-  <div class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      {{-- <div class="mb-10">
-        <a href="{{ route('admin.bookings.create') }}"
-           class="px-4 py-2 font-bold text-white bg-green-500 rounded shadow-lg hover:bg-green-700">
-          + Tambah Item
-        </a>
-      </div> --}}
-      <div class="overflow-hidden shadow sm:rounded-md">
-        <div class="px-4 py-5 bg-white sm:p-6">
-          <table id="dataTable" class="border cell-border compact stripe">
-            <thead >
-              <tr>
-                <th style="">ID</th>
-                <th style="">Nama Akun</th>
-                <th>Nama Pemesan</th>
-                <th>Brand</th>
-                <th>Item</th>
-                <th>Mulai</th>
-                <th>Selesai</th>
-                <th>Status Booking</th>
-                <th>Status Pembayaran</th>
-                <th>Total Dibayar</th>
-                
-                <th style="max-width: 1%">Aksi</th>
-              </tr>
-            </thead>
-            <tbody class="text-center">
-              
-            </tbody>
-          </table>
         </div>
-      </div>
     </div>
-  </div>
+
+    <x-slot name="script">
+        <script>
+            // AJAX DataTable
+            const datatable = $('#dataTable').DataTable({
+                processing: true,
+                serverSide: true,
+                stateSave: true,
+                ajax: {
+                    url: '{!! url()->current() !!}',
+                    data: function (d) {
+                        d.statusBooking = $('#statusBooking').val()
+                        d.statusBayar = $('#statusBayar').val()
+                        d.startDate = $('#startDate').val()
+                        d.endDate = $('#endDate').val()
+                    }
+                },
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.12.1/i18n/id.json'
+                },
+
+                columns: [
+                    //{ className: 'dt-center', targets: '_all' },
+                    {
+                        data: 'id',
+                        name: 'id',
+                    },
+                    {
+                        data: 'user.name',
+                        name: 'user.name',
+                        // orderable: false,
+                        // searchable: false,
+                        // width: '20%',
+                    },
+                    {
+                        data: 'name',
+                        name: 'name',
+                        // orderable: false,
+                        // searchable: false,
+                        // width: '20%',
+                    },
+                    {
+                        data: 'item.brand.name',
+                        name: 'item.brand.name'
+                    },
+
+                    {
+                        data: 'item.name',
+                        name: 'item.name'
+                    },
+                    {
+                        data: 'start_date',
+                        name: 'start_date',
+                        width: '10%',
+
+
+                    },
+                    {
+                        data: 'end_date',
+                        name: 'end_date',
+                        width: '10%',
+
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+
+                    },
+                    {
+                        data: 'payment_status',
+                        name: 'payment_status'
+
+                    },
+                    {
+                        data: 'total_price',
+                        name: 'total_price',
+                        width: '10%',
+                        render: $.fn.dataTable.render.number('.', ',', 0, 'Rp '),
+
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false,
+                        width: '15%'
+                    },
+                ],
+            });
+
+            $('#statusBooking').change(function () {
+                datatable.draw();
+            });
+
+            $('#statusBayar').change(function () {
+                datatable.draw();
+            });
+
+
+            $('#startDate').change(function () {
+                datatable.draw();
+            });
+
+            $('#endDate').change(function () {
+                datatable.draw();
+            });
+
+
+        </script>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            {{-- <div class="mb-10">
+              <a href="{{ route('admin.bookings.create') }}"
+                 class="px-4 py-2 font-bold text-white bg-green-500 rounded shadow-lg hover:bg-green-700">
+                + Tambah Item
+              </a>
+            </div> --}}
+            <div class="overflow-hidden shadow sm:rounded-md">
+                <div class="px-4 py-5 bg-white sm:p-6">
+                    <table id="dataTable" class="border cell-border compact stripe">
+                        <thead>
+                        <tr>
+                            <th style="">ID</th>
+                            <th style="">Nama Akun</th>
+                            <th>Nama Pemesan</th>
+                            <th>Brand</th>
+                            <th>Item</th>
+                            <th>Mulai</th>
+                            <th>Selesai</th>
+                            <th>Status Booking</th>
+                            <th>Status Pembayaran</th>
+                            <th>Total Dibayar</th>
+
+                            <th style="max-width: 1%">Aksi</th>
+                        </tr>
+                        </thead>
+                        <tbody class="text-center">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
