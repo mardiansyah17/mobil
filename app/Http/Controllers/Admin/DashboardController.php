3ca24991830dc9206399e3bc5833a0c60689b885
Mardiansyah17 <mardiansyahm002@gmail.com>
2023-12-31 23:20:36 +0700

    up

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+//        $kasMasuk = KasMasuk::select(DB::raw('MONTH(tanggal) as month, YEAR(tanggal) as year'), DB::raw('COUNT(*) as count'))
+//            ->groupBy(DB::raw('YEAR(tanggal), MONTH(tanggal)'))
+//            ->get();
+//        dd($kasMasuk);
         return view('admin.dashboard');
     }
 }
