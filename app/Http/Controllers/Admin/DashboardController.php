de9f9da16eefbac7a2fceb726b69a90db49decac
Mardiansyah17 <mardiansyahm002@gmail.com>
2024-01-11 22:42:18 +0700

    up

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,37 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Booking;
 use App\Models\KasKeluar;
 use App\Models\KasMasuk;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $x = [
-            [
-                "month" => 10,
-                "year" => 2023,
-                "count" => "2000000"
-            ],
-            [
-                "month" => 10,
-                "year" => 2023,
-                "count" => "2000000"
-            ]
-        ];
-
-     $yy = [
-            [
-                "month" => 12,
-                "year" => 2023,
-                "count" => "2000000"
-            ],
-            [
-                "month" => 10,
-                "year" => 2023,
-                "count" => "2000000"
-            ]
-        ];
 
 
         $denda = Booking::where('denda', '!=', 0)->select(DB::raw('MONTH(start_date) as month, YEAR(start_date) as year'), DB::raw('sum(denda) as count'))
@@ -45,14 +21,33 @@ class DashboardController extends Controller
         $kasMasuk = KasMasuk::select(DB::raw('MONTH(tanggal) as month, YEAR(tanggal) as year'), DB::raw('sum(total) as count'))
             ->groupBy(DB::raw('YEAR(tanggal), MONTH(tanggal)'))
             ->get()->toArray();
-//        dd($kasMasuk);
+
+//        dd($kasMasuk, $denda);
+        // Menggabungkan array $x dan $y
+        $result = Collection::make($denda)->merge($kasMasuk);
+
+// Mengelompokkan berdasarkan month dan year
+        $grouped = $result->groupBy(function ($item) {
+            return $item['month'] . '-' . $item['year'];
+        });
+
+// Menggabungkan array yang memiliki month dan year yang sama
+        $mergedKasMasuk = $grouped->map(function ($group) {
+            return [
+                "month" => $group[0]['month'],
+                "year" => $group[0]['year'],
+                "count" => $group->sum('count'),
+            ];
+        })->values()->all();
+
+
         $kasKeluar = KasKeluar::select(DB::raw('MONTH(tanggal) as month, YEAR(tanggal) as year'), DB::raw('sum(total) as count'))
             ->groupBy(DB::raw('YEAR(tanggal), MONTH(tanggal)'))
             ->get()->toArray();
 
         $pemasukan = Booking::query()->where('status', '=', 'done')->get();
         return view('admin.dashboard', [
-            'kasMasuk' => $kasMasuk,
+            'kasMasuk' => $mergedKasMasuk,
             'kasKeluar' => $kasKeluar,
             'pemasukan' => $pemasukan
         ]);
