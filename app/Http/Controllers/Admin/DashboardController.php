bb00288c2edf1634319f739aaafad88d1ceff50f
mardiansyah17 <mardiansyahm002@gmail.com>
2024-01-11 20:03:33 +0700

    f

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,6 +12,36 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $x = [
+            [
+                "month" => 10,
+                "year" => 2023,
+                "count" => "2000000"
+            ],
+            [
+                "month" => 10,
+                "year" => 2023,
+                "count" => "2000000"
+            ]
+        ];
+
+     $yy = [
+            [
+                "month" => 12,
+                "year" => 2023,
+                "count" => "2000000"
+            ],
+            [
+                "month" => 10,
+                "year" => 2023,
+                "count" => "2000000"
+            ]
+        ];
+
+
+        $denda = Booking::where('denda', '!=', 0)->select(DB::raw('MONTH(start_date) as month, YEAR(start_date) as year'), DB::raw('sum(denda) as count'))
+            ->groupBy(DB::raw('YEAR(start_date), MONTH(start_date)'))->get()->toArray();
+
         $kasMasuk = KasMasuk::select(DB::raw('MONTH(tanggal) as month, YEAR(tanggal) as year'), DB::raw('sum(total) as count'))
             ->groupBy(DB::raw('YEAR(tanggal), MONTH(tanggal)'))
             ->get()->toArray();
